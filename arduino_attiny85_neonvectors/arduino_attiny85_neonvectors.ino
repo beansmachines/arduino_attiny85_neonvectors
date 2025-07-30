921782c444e4cd551026b713a0ef7f4ed1b41ef1
Chris <chris.bean7@gmail.com>
2025-07-31 02:37:00 +1200

    added arduino folder and files but library issues on vscode

---
+++ arduino_attiny85_neonvectors/arduino_attiny85_neonvectors.ino
@@ -0,0 +1,211 @@
+
+//USE ONE SET OF INCLUDES FOR YOUR IDE
+//Arduino IDE
+/*
+#include <Adafruit_NeoPixel.h> //Adafruit Neopixel by Adafruit version 1.11.0 installed (in Arduino IDE, just have the .h file name without the folder)
+#include <EEPROM.h> //arduino native probably
+*/
+
+//vscode with arduino maker workshop
+#include <./Adafruit_NeoPixel/Adafruit_NeoPixel.h> //Adafruit Neopixel by Adafruit version 1.11.0 installed (in Arduino IDE, just have the .h file name without the folder)
+#include <C:\Users\chris\AppData\Local\Arduino15\packages\ATTinyCore\hardware\avr\1.5.2\libraries\EEPROM\EEPROM.h> //arduino native probably
+#include <C:\Users\chris\AppData\Local\Arduino15\packages\arduino\tools\avr-gcc\7.3.0-atmel3.6.1-arduino7\avr\include\inttypes.h> //required for vscode
+#include <C:\Users\chris\AppData\Local\Arduino15\packages\arduino\tools\avr-gcc\7.3.0-atmel3.6.1-arduino7\avr\include\stdint.h> //required for vscode
+//#include <C:\Users\chris\AppData\Local\Arduino15\packages\arduino\tools\avr-gcc\7.3.0-atmel3.6.1-arduino7\lib\gcc\avr\7.3.0\include\stdint.h>
+
+// set to pin connected to data input of WS8212 (NeoPixel) strip
+#define PIN         2
+
+// any pin with analog input (used to initialize random number generator)
+#define RNDPIN      3
+
+// number of LEDs (NeoPixels) in your strip
+// (please note that you need 3 bytes of RAM available for each pixel)
+#define NUMPIXELS   8
+// max LED brightness (1 to 255) – start with low values!
+// (please note that high brightness requires a LOT of power)
+#define BRIGHTNESS  255
+
+// increase to get narrow spots, decrease to get wider spots
+#define FOCUS       65
+
+// decrease to speed up, increase to slow down (it's not a delay actually)
+#define DELAY       2000
+
+// set to 1 to display FPS rate
+//#define DEBUG       0
+
+// if true, wrap color wave over the edge (used for circular stripes)
+#define WRAP        1
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+// we have 3 color spots (reg, green, blue) oscillating along the strip with different speeds
+float spdr, spdg, spdb;
+float offset;
+
+// #if DEBUG
+// // track fps rate
+// long nextms = 0;
+// int pfps = 0, fps = 0;
+// #endif
+
+// the real exponent function is too slow, so I created an approximation (only for x < 0)
+float myexp(float x) {
+  return (1.0/(1.0-(0.634-1.344*x)*x));
+}
+
+
+//eeprom
+int eepromaddress = 0;
+int eepromreadvalue1;
+int eepromreadvalue2;
+uint8_t eepromwritevalue;
+bool neonmode = false;
+bool gentleglowmode = false;
+
+void setup() {
+
+  eepromroutine();
+  
+  // initialize pseudo-random number generator with some random value
+  randomSeed(analogRead(RNDPIN));
+
+  // assign random speed to each spot
+  spdr = 1.0 + random(200) / 100.0;
+  spdg = 1.0 + random(200) / 100.0;
+  spdb = 1.0 + random(200) / 100.0;
+
+  // set random offset so spots start in random locations
+  offset = random(10000) / 100.0;
+
+  // initialize LED strip
+  strip.begin();
+  strip.show();
+
+  if(neonmode == true){
+    //neonmode
+    neonmodeloop();
+  }
+
+  if(gentleglowmode == true){
+      //gentle glow mode
+      gentlegloweffect();
+  }
+
+}
+
+void loop() {
+
+
+
+  // use real time to recalculate position of each color spot
+  long ms = millis();
+  // scale time to float value
+  float m = offset + (float)ms/DELAY;
+  // add some non-linearity
+  m = m - 42.5*cos(m/552.0) - 6.5*cos(m/142.0);
+
+  // recalculate position of each spot (measured on a scale of 0 to 1)
+  float posr = 0.15 + 0.55*sin(m*spdr);
+  float posg = 0.5 + 0.65*sin(m*spdg);
+  float posb = 0.85 + 0.75*sin(m*spdb);
+
+  // now iterate over each pixel and calculate it's color
+  for (int i=0; i<NUMPIXELS; i++) {
+    // pixel position on a scale from 0.0 to 1.0
+    float ppos = (float)i / NUMPIXELS;
+ 
+    // distance from this pixel to the center of each color spot
+    float dr = ppos-posr;
+    float dg = ppos-posg;
+    float db = ppos-posb;
+#if WRAP
+    dr = dr - floor(dr + 0.5);
+    dg = dg - floor(dg + 0.5);
+    db = db - floor(db + 0.5);
+#endif
+
+    // set each color component from 0 to max BRIGHTNESS, according to Gaussian distribution
+    strip.setPixelColor(i,
+      constrain(BRIGHTNESS*myexp(-FOCUS*dr*dr),0,BRIGHTNESS),
+      constrain(BRIGHTNESS*myexp(-FOCUS*dg*dg),0,BRIGHTNESS),
+      constrain(BRIGHTNESS*myexp(-FOCUS*db*db),0,BRIGHTNESS)
+      );
+  }
+
+// #if DEBUG
+//   // keep track of FPS rate
+//   fps++;
+//   if (ms>nextms) {
+//     // 1 second passed – reset counter
+//     nextms = ms + 1000;
+//     pfps = fps;
+//     fps = 0;
+//   }
+//   // show FPS rate by setting one pixel to white
+//   strip.setPixelColor(pfps,BRIGHTNESS,BRIGHTNESS,BRIGHTNESS);
+// #endif
+
+  // send data to LED strip
+  strip.show();
+}
+
+
+void eepromroutine(){
+  //EEPROM IS EMULATED ON CLONE ARDUINOS and won't survive a power-cycle; however it will work when pressing reset button  
+  EEPROM.get(eepromaddress, eepromreadvalue1);
+  //Serial.print("eeprom read 1: ");
+  //Serial.println(eepromreadvalue1);
+
+  //for power on or having issues
+  if(eepromreadvalue1 == 0 || eepromreadvalue1 == 1 || eepromreadvalue1 == 2){
+    //only force zero if is not one of the desired values
+  } else {EEPROM.put(eepromaddress, 0);}
+  
+  if(eepromreadvalue1 == 0){
+    neonmode = false;
+    gentleglowmode = false;
+    eepromwritevalue = eepromreadvalue1 + 1;
+    EEPROM.put(eepromaddress, eepromwritevalue);
+  } else if(eepromreadvalue1 == 1){
+    neonmode = true;
+    gentleglowmode = false;
+    eepromwritevalue = eepromreadvalue1 + 1;
+    EEPROM.put(eepromaddress, eepromwritevalue);
+  } else if(eepromreadvalue1 == 2){
+    neonmode = false;
+    gentleglowmode = true;
+    eepromwritevalue = eepromreadvalue1 - 2;
+    EEPROM.put(eepromaddress, eepromwritevalue);
+  }
+  // Serial.print("rainbow mode: ");
+  // Serial.println(neonmode);
+  // Serial.print("gentle glow mode: ");
+  // Serial.println(gentleglowmode);
+
+  EEPROM.get(eepromaddress, eepromreadvalue2);
+  // Serial.print("eeprom read 2: ");
+  // Serial.println(eepromreadvalue2);
+}
+
+void neonmodeloop(){
+  while(1==1){
+    for(int i=0; i<8; i++){
+      strip.setPixelColor(i,255,40,100);
+    }
+    strip.show();
+    delay(5000);
+  }
+}
+  
+void gentlegloweffect(){
+    while(1==1){
+    for(int i=0; i<8; i++){
+      strip.setPixelColor(i,0,255,200);
+    }
+    strip.show();
+    delay(5000);
+  }
+}
